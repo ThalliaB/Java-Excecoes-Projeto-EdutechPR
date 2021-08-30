62884457f42c6198230d7723abb7f59831e4c013
ThalliaB <thalliabrum60@gmail.com>
2021-08-30 18:40:13 -0300

    Add ecxeptions test and how throw exceptions

--- Java pilha/src/FluxoComTratamento.java
+++ Java pilha/src/FluxoComTratamento.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -20,9 +20,12 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-       
-        throw new ArithmeticException();
-        
-        //System.out.println("Fim do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            int a = i / 0;
+//            Conta c = null;
+//            c.deposita();
+        }
+        System.out.println("Fim do metodo2");
     }
 }
