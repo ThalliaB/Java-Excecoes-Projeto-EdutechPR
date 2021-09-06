1e4eec67ee0412040130160d7e0f77e6beba4f1b
ThalliaB <thalliabrum60@gmail.com>
2021-09-06 10:05:44 -0300

    Create Test class

---
+++ Java pilha/src/TestaContaComExceptionChecked.java
@@ -0,0 +1,16 @@
+
+public class TestaContaComExceptionChecked {
+
+	public static void main(String[] args) {
+		
+		Conta c = new Conta();
+		
+		try {
+			c.deposita();			
+		} catch(MyException ex) {
+			System.out.println("Tratamento");
+		}
+		
+	}
+	
+}
